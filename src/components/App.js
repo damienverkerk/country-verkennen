d79d8fdb438fbc5298b96d6350de61af53aeef97
damienverkerk <damien-verkerk@hotmail.com>
2023-09-29 16:19:34 +0200

    Components opgebouwd met basis structuur en de daarbij behorende styling toegevoegd

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import '../styles/app.css';
 import Header from './Header';
 import Footer from './Footer';
+import Login from './Login';
+import Register from './Register';
+import CountryList from './CountryList';
+import Country from './Country';
+import Score from './Score';
 
 function App() {
     return (
@@ -9,6 +14,22 @@ function App() {
             <Header />
             <main>
                 <h2>Welkom bij Mijn Reisapp!</h2>
+                <Login />
+                <Register />
+                <CountryList />
+                <Country
+                    name="Nederland"
+                    capital= "Amsterdam"
+                    population= "17 miljoen"
+                />
+                <Score country="Nederland" score="8.5" />
+                <Country
+                    name="Belgie"
+                    capital="Brussel"
+                    population="11 miljoen"
+                    />
+                <Score country="Belgie" score="7.5" />
+                
                 {/* Hier komen andere componenten en inhoud */}
             </main>
             <Footer />
--- src/components/Score.js
+++ src/components/Score.js
@@ -1,1 +1,12 @@
+import React from 'react';
 import '../styles/score.css';
+
+function Score({country, score}){
+    return(
+        <div className='score-container'>
+            <h4>{country}</h4>
+            <p>Score: {score}</p>
+        </div>
+    )
+}
+export default Score;
