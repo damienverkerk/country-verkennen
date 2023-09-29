52400d92bf8539b46fd61047ea917b095d4d5a5b
damienverkerk <damien-verkerk@hotmail.com>
2023-09-29 19:43:02 +0200

    countryList dummy opmaak

--- src/components/features/CountryList/index.js
+++ src/components/features/CountryList/index.js
@@ -1,1 +1,23 @@
- 
+import React, {useState, useEffect} from 'react';
+ import '../../../styles/countryList.css';
+
+ function CountryList() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        // Fetch countries from API here
+    }, [])
+
+    return (
+        <div className='country-list'>
+            <h2>Countries</h2>
+            <ul>
+                {countries.map(country => (
+                    <li key={country.code}>{country.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+ }
+
+ export default CountryList;
