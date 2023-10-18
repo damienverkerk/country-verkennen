ca60ca2bf337d2c6be5079ed2143c5b31d7b6049
damienverkerk <damien-verkerk@hotmail.com>
2023-10-18 23:40:48 +0200

    settings component toegevoegd met styling

---
+++ src/components/features/Settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Settings.css';
+
+function Settings() {
+    return (
+        <div className="settings-container">
+            <h1>Account Settings</h1>
+            <p>Manage your personal preferences and account details.</p>
+        </div>
+    );
+}
+
+export default Settings;
